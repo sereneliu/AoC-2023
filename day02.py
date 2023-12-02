9c450ef61e6f7ff1f28f661ec52e03b2495bdc29
sereneliu <>
2023-12-02 21:30:20 +0000

    Day 2 Part 2 completed

--- day02.py
+++ day02.py
@@ -27,15 +27,19 @@ def parse_data(data):
 
 parse_data(puzzle_input)
 
-def find_possible(game_key, game_value, max_blue, max_green, max_red):
+def find_max(colors):
     blues = []
     greens = []
     reds = []
-    for game_set in game_value:
+    for game_set in colors:
         blues.append(game_set[0])
         greens.append(game_set[1])
         reds.append(game_set[2])
-    if max_blue >= max(blues) and max_green >= max(greens) and max_red >= max(reds):
+    return max(blues), max(greens), max(reds)
+
+def find_possible(game_key, game_value, max_blue, max_green, max_red):
+    max_blues, max_greens, max_reds = find_max(game_value)
+    if max_blue >= max_blues and max_green >= max_greens and max_red >= max_reds:
         return game_key
     else:
         return 0
@@ -46,4 +50,13 @@ def find_game_id_sum(game_dict):
         game_id_sum += find_possible(key, value, 14, 13, 12)
     return game_id_sum
 
+def part_two(game_dict):
+    power_sum = 0
+    for key, value in game_dict.items():
+        b, g, r = find_max(value)
+        power = b * g * r
+        power_sum += power
+    return power_sum
+
 print(find_game_id_sum(games))
+print(part_two(games))
