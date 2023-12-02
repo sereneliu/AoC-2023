a7f1574d005e46cdb4995b53d60c62ad2f362b77
sereneliu <>
2023-12-02 21:53:00 +0000

    Day 1 Part 1 completed

--- day01.py
+++ day01.py
@@ -1,2 +1,15 @@
+import re
 from aocd import get_data
 puzzle_input = get_data(day=1, year=2023).split('\n')
+
+def find_calibration(text):
+    numbers = re.findall('\d', text)
+    return int(numbers[0] + numbers[-1])
+
+def find_calibration_total(doc):
+    total = 0
+    for line in doc:
+        total += find_calibration(line)
+    return total
+
+print(find_calibration_total(puzzle_input))
