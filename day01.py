e4e26dc83a65af8de1081a3afc67f788d469d10a
sereneliu <>
2023-12-02 22:39:09 +0000

    Day 1 Part 2 WIP

--- day01.py
+++ day01.py
@@ -1,6 +1,8 @@
 import re
+from word2number import w2n
 from aocd import get_data
-puzzle_input = get_data(day=1, year=2023).split('\n')
+
+puzzle_input = get_data(day=1, year=2023)
 
 def find_calibration(text):
     numbers = re.findall('\d', text)
@@ -8,8 +10,16 @@ def find_calibration(text):
 
 def find_calibration_total(doc):
     total = 0
-    for line in doc:
+    for line in doc.split('\n'):
         total += find_calibration(line)
     return total
 
 print(find_calibration_total(puzzle_input))
+
+def replace_word(doc):
+    new_doc = doc
+    for num_word, num_int in w2n.american_number_system.items():
+        new_doc = re.sub(num_word, str(num_int), new_doc, re.M)
+    return new_doc
+
+print(find_calibration_total(replace_word(puzzle_input)))
