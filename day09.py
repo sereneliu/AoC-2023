eb1bb272457a9e89fdfc82b547f2a4b6ed748e9d
Serene Liu <github@serenely.me>
2023-12-09 14:10:25 -0500

    Day 9 Part 1 completed

--- day09.py
+++ day09.py
@@ -1,2 +1,25 @@
 from aocd import get_data
 puzzle_input = get_data(day=9, year=2023).split('\n')
+
+def find_difference(history):
+    difference = []
+    for idx in range(len(history) - 1):
+        difference.append(history[idx + 1] - history[idx])
+    return difference
+
+def find_next_value(history):
+    difference = history
+    last_value = history[-1]
+    while set(difference) != {0}:
+        difference = find_difference(difference)
+        last_value += difference[-1]
+    return last_value
+
+def find_sum(dataset):
+    sum = 0
+    for history in dataset:
+        history = [int(x) for x in history.split(' ')]
+        sum += find_next_value(history)
+    return sum
+
+print(find_sum(puzzle_input))
