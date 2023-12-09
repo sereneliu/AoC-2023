71fdb73882bda6de0016bb67701d4940baf37540
Serene Liu <github@serenely.me>
2023-12-09 14:43:32 -0500

    Day 9 Part 2 completed

--- day09.py
+++ day09.py
@@ -9,17 +9,25 @@ def find_difference(history):
 
 def find_next_value(history):
     difference = history
+    first_values = [difference[0]]
     last_value = history[-1]
     while set(difference) != {0}:
         difference = find_difference(difference)
+        first_values.append(difference[0])
         last_value += difference[-1]
-    return last_value
+    first_value = first_values[-1]
+    while len(first_values) >= 2:
+        first_value = first_values[-2] - first_value
+        first_values.pop()
+    return first_value, last_value
 
 def find_sum(dataset):
-    sum = 0
+    part_1_sum = 0
+    part_2_sum = 0
     for history in dataset:
         history = [int(x) for x in history.split(' ')]
-        sum += find_next_value(history)
-    return sum
+        part_1_sum += find_next_value(history)[0]
+        part_2_sum += find_next_value(history)[1]
+    return part_1_sum, part_2_sum
 
 print(find_sum(puzzle_input))
